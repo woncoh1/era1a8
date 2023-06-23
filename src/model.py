a2e59db27c95d23bfd4e1cbf2f32d2da0e7458a8
Chance <woncoh1@gmail.com>
2023-06-23 23:12:32 +0900

    Add layers

---
+++ src/model.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+
+
+def norm(
+    n:str, # Normalization type
+    o:int, # num_channels
+    g:int, # num_groups
+) -> nn.modules:
+    NORMS = ['bn', 'gn', 'ln',]
+    assert n in NORMS, f"n should be one of {NORMS}"
+    if n == 'bn': return nn.BatchNorm2d(o)
+    if n == 'gn': return nn.GroupNorm(g, o)
+    if n == 'ln': return nn.GroupNorm(1, o)
+
+
+def conv( # Convolution layer: 3x3 convolution to extract features
+    i:int, # in_channels
+    o:int, # out_channels
+    n:str, # Normalization type
+    g:int, # num_groups
+    d:float, # Dropout rate
+    p:int=1, # padding
+) -> nn.Sequential:
+    return nn.Sequential(
+        nn.Conv2d(
+            in_channels=i,
+            out_channels=o,
+            kernel_size=3,
+            stride=1,
+            padding=p,
+            padding_mode='replicate',
+            bias=False,
+        ),
+        norm(n, o, g=g),
+        nn.Dropout2d(p=d),
+        nn.ReLU(),
+    )
+
+
+class SkipBlock(nn.Module):
+    # https://engineering.purdue.edu/DeepLearn/pdf-kak/SkipConsAndBN.pdf
+    # https://engineering.purdue.edu/kak/distDLS/DLStudio-2.3.0_CodeOnly.html
+    def __init__(self,
+        i:int, # in_channels
+        o:int, # out_channels
+        n:str, # Normalization type
+        g:int, # num_groups
+        d:float, # Dropout rate
+        down=False, # Downsampling
+        skip=True, # Skip connection
+    ):
+        super().__init__()
+        self.down = down
+        self.skip = skip
+        self.i = i
+        self.o = o
+        self.conv1 = nn.Conv2d(i, o, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(i, o, 3, stride=1, padding=1)
+        self.norm1 = norm(n, o, g=g)
+        self.norm2 = norm(n, o, g=g)
+        self.drop1 = nn.Dropout2d(p=d)
+        self.drop2 = nn.Dropout2d(p=d)
+        if down:
+            self.downsampler = nn.Conv2d(i, o, 1, stride=2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.drop1(out)
+        out = nn.functional.relu(out)
+        if self.i == self.o:
+            out = self.conv2(out)
+            out = self.norm2(out)
+            out = self.drop2(out)
+        if self.down:
+            out = self.downsampler(out)
+            identity = self.downsampler(identity)
+        if self.skip:
+            if self.i == self.o:
+                out = out + identity
+            else:
+                out = torch.cat([
+                    #  [B,C      ,H,W]
+                    out[:,:self.i,:,:] + identity,
+                    out[:,self.i:,:,:] + identity,
+                ], dim=1)
+        out = nn.functional.relu(out)
+        return out
+
+
+class Net(nn.Module):
+    def __init__(self,
+        norm:str='bn',
+        grps:int=1,
+        drop:float=0,
+    ) -> None:
+        super().__init__()
+        self.conv1 = conv(3, 8, norm, grps, drop)
+        self.conv2 = SkipBlock(8, 8, norm, grps, drop)
+        self.tran1 = SkipBlock(8, 8, norm, grps, drop, down=True)
+        self.conv3 = SkipBlock(8, 8, norm, grps, drop)
+        self.conv4 = SkipBlock(8, 16, norm, grps, drop)
+        self.conv5 = SkipBlock(16, 16, norm, grps)
+        self.tran2 = SkipBlock(16, 16, norm, grps, drop, down=True)
+        self.conv6 = SkipBlock(16, 16, norm, grps, drop)
+        self.conv7 = SkipBlock(16, 32, norm, grps, drop)
+        self.conv8 = SkipBlock(32, 32, norm, grps, drop)
+        self.tran3 = last(32, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.tran1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.tran2(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.tran3(x)
+        return x
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,93 @@ import torch
 import matplotlib.pyplot as plt
 
 
-def inspect_batch(dataloader: torch.utils.data.DataLoader) -> None:
-    """Visualize sample images from a batch."""
+def count_correct_predictions(
+    predictions:torch.Tensor,
+    labels:torch.Tensor,
+) -> int:
+    return predictions.argmax(dim=1).eq(labels).sum().item()
+
+
+def get_incorrect_predictions(
+    device:torch.device,
+    loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    criterion:torch.nn.functional,
+) -> list:
+    """Get all incorrect predictions."""
+    model.eval()
+    incorrects = []
+    with torch.no_grad():
+        for data, target in loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            loss = criterion(output, target)
+            pred = output.argmax(dim=1)
+            for d, t, p, o in zip(data, target, pred, output):
+                if p.eq(t.view_as(p)).item() == False:
+                    incorrects.append([
+                        d.cpu(),
+                        t.cpu(),
+                        p.cpu(),
+                        o[p.item()].cpu(),
+                    ])
+    return incorrects
+
+
+def find_lr(
+    device:torch.device,
+    train_loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    criterion:torch.nn.functional,
+    optimizer:torch.optim.Optimizer,
+    init_value:float=1e-8,
+    final_value:float=10.,
+    beta: float=0.98,
+) -> tuple[list[float], list[float]]:
+    """https://sgugger.github.io/how-do-you-find-a-good-learning-rate.html"""
+    trn_loader = train_loader
+    net = model
+    num = len(trn_loader) - 1
+    mult = (final_value / init_value) ** (1/num)
+    lr = init_value
+    optimizer.param_groups[0]['lr'] = lr
+    avg_loss = 0.
+    best_loss = 0.
+    batch_num = 0
+    losses = []
+    lrs = []
+    for data in trn_loader:
+        batch_num += 1
+        # Get the loss for this mini-batch of inputs/outputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        # Compute the smoothed loss
+        avg_loss = beta*avg_loss+(1-beta)*loss.item()
+        smoothed_loss = avg_loss/(1-beta**batch_num)
+        # Stop if the loss is exploding
+        if batch_num > 1 and smoothed_loss > 4*best_loss:
+            return lrs, losses
+        # Record the best loss
+        if smoothed_loss < best_loss or batch_num==1:
+            best_loss = smoothed_loss
+        # Store the values
+        losses.append(smoothed_loss)
+        lrs.append(lr)
+        # Do the SGD step
+        loss.backward()
+        optimizer.step()
+        # Update the lr for the next step
+        lr *= mult
+        optimizer.param_groups[0]['lr'] = lr
+    return lrs, losses
+
+
+def plot_batch(dataloader: torch.utils.data.DataLoader) -> None:
+    """Plot sample images from a batch."""
     batch_data, batch_label = next(iter(dataloader))
     fig = plt.figure()
     for i in range(12):
@@ -53,3 +138,23 @@ def plot_curves(results: dict[str, list[float]], epoch:int) -> None:
     plt.setp(axs, xticks=epochs)
     plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     plt.show()
+
+
+def plot_incorrect_predictions(
+    predictions:list,
+    classes:list,
+    count:int=10,
+) -> None:
+    """Plot incorrect predictions."""
+    print(f'Total Incorrect Predictions {len(predictions)}')
+    if not count % 5 == 0:
+        print("Count should be multiple of 10")
+        return
+    # classes = list(range(len(classes)))
+    fig = plt.figure(figsize=(10, 5))
+    for i, (d, t, p, o) in enumerate(predictions):
+        ax = fig.add_subplot(int(count/5), 5, i + 1, xticks=[], yticks=[])
+        ax.set_title(f'Guess: {classes[p.item()]}\nRight: {classes[t.item()]}')
+        plt.imshow(d.cpu().numpy().transpose(1, 2, 0))
+        if i+1 == 5*(count/5):
+            break
