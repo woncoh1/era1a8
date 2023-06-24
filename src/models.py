04c28e930a58013111d895020de8982bfdb2e077
Chance <woncoh1@gmail.com>
2023-06-24 17:29:39 +0900

    Recover original SKipBlock

--- src/models.py
+++ src/models.py
@@ -64,14 +64,14 @@ class SkipBlock(nn.Module):
         self.skip = skip
         self.i = i
         self.o = o
-        self.conv1 = nn.Conv2d(i, o, 3, stride=1, padding=1, padding_mode='replicate', bias=True)
-        self.conv2 = nn.Conv2d(i, o, 3, stride=1, padding=1, padding_mode='replicate', bias=True)
+        self.conv1 = nn.Conv2d(i, o, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(i, o, 3, stride=1, padding=1)
         self.norm1 = norm(n, o, g=g)
         self.norm2 = norm(n, o, g=g)
         self.drop1 = nn.Dropout2d(p=d)
         self.drop2 = nn.Dropout2d(p=d)
         if down:
-            self.downsampler = nn.Conv2d(i, o, 1, stride=2, bias=True)
+            self.downsampler = nn.Conv2d(i, o, 1, stride=2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
