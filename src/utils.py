6939888bf79f23e1a2ce9f997f351a7cb384aed8
Chance <woncoh1@gmail.com>
2023-06-25 21:40:12 +0900

    Remove initialization

--- src/utils.py
+++ src/utils.py
@@ -3,19 +3,6 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 
 
-def initialize_weights(
-    m: nn.Module
-) -> None:
-    """Initialize weights of convolution and normalization layers.
-    https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L208-L213
-    """
-    if isinstance(m, nn.Conv2d):
-        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-    elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.GroupNorm):
-        nn.init.constant_(m.weight, 1)
-        nn.init.constant_(m.bias, 0)
-
-
 def get_incorrect_predictions(
     device:torch.device,
     loader:torch.utils.data.DataLoader,
