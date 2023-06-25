16c270c510da524544388601b0914a548151ea9f
Chance <woncoh1@gmail.com>
2023-06-25 16:01:14 +0900

    Rename functions and increase model parameters

--- src/models.py
+++ src/models.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 
 
-def norm( # Normalization layer: batch, layer, or group
+def normlayer( # Normalization layer: batch, layer, or group
     n:str, # Normalization type
     o:int, # num_channels
-    g:int, # num_groups
-) -> nn.modules:
-    NORMS = ['bn', 'ln', 'gn',]
+    g:int=1, # num_groups
+) -> nn.BatchNorm2d|nn.GroupNorm:
+    NORMS = ['bn', 'gn', 'ln']
     assert n in NORMS, f"n should be one of {NORMS}"
     if n == 'bn': return nn.BatchNorm2d(o)
-    if n == 'ln': return nn.GroupNorm(1, o)
     if n == 'gn': return nn.GroupNorm(g, o)
+    if n == 'ln': return nn.GroupNorm(1, o)
 
 
-def conv( # Convolution layer: 3x3 convolution to extract features
+def convblock( # Convolution block: 3x3 convolution to extract features
     i:int, # in_channels
     o:int, # out_channels
     n:str, # Normalization type
@@ -23,14 +23,14 @@ def conv( # Convolution layer: 3x3 convolution to extract features
     p:int=1, # padding
 ) -> nn.Sequential:
     return nn.Sequential(
-        nn.Conv2d(i, o, 3, stride=1, padding=p),
-        norm(n, o, g=g),
+        nn.Conv2d(i, o, 3, stride=1, padding=p, bias=False),
+        normlayer(n, o, g=g),
         nn.Dropout2d(p=d),
         nn.ReLU(),
     )
 
 
-def last( # Prediction layer = GAP + softmax
+def predblock( # Prediction block = GAP + softmax
     i:int, # in_channels
     o:int, # out_channels
 ) -> nn.Sequential:
@@ -38,7 +38,7 @@ def last( # Prediction layer = GAP + softmax
         # [-1, i, s, s]
         nn.AdaptiveAvgPool2d(output_size=1),
         # [-1, i, 1, 1]
-        nn.Conv2d(i, o, 1, stride=1),
+        nn.Conv2d(i, o, 1, stride=1, padding=0, bias=False),
         # [-1, o, 1, 1]
         nn.Flatten(),
         # [-1, o]
@@ -48,30 +48,28 @@ def last( # Prediction layer = GAP + softmax
 
 
 class SkipBlock(nn.Module):
-    # https://engineering.purdue.edu/DeepLearn/pdf-kak/SkipConsAndBN.pdf
-    # https://engineering.purdue.edu/kak/distDLS/DLStudio-2.3.0_CodeOnly.html
     def __init__(self,
         i:int, # in_channels
         o:int, # out_channels
         n:str, # Normalization type
-        g:int, # num_groups
+        g:int, # Normalization group count
         d:float, # Dropout rate
-        down=False, # Downsampling
-        skip=True, # Skip connection
+        down=False, # Downsample
     ) -> None:
         super().__init__()
-        self.down = down
-        self.skip = skip
         self.i = i
         self.o = o
-        self.conv1 = nn.Conv2d(i, o, 3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(i, o, 3, stride=1, padding=1)
-        self.norm1 = norm(n, o, g=g)
-        self.norm2 = norm(n, o, g=g)
+        self.down = down
+        self.conv1 = nn.Conv2d(i, o, 3, stride=2 if down else 1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(o, o, 3, stride=1, padding=1, bias=False)
+        self.norm1 = normlayer(n, o, g=g)
+        self.norm2 = normlayer(n, o, g=g)
         self.drop1 = nn.Dropout2d(p=d)
         self.drop2 = nn.Dropout2d(p=d)
+        if i != o:
+            self.downsampler = nn.Conv2d(i, o, 1, stride=1, padding=0, bias=False)
         if down:
-            self.downsampler = nn.Conv2d(i, o, 1, stride=2)
+            self.downsampler = nn.Conv2d(i, o, 1, stride=2, padding=0, bias=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
@@ -79,44 +77,34 @@ class SkipBlock(nn.Module):
         out = self.norm1(out)
         out = self.drop1(out)
         out = nn.functional.relu(out)
-        if self.i == self.o:
-            out = self.conv2(out)
-            out = self.norm2(out)
-            out = self.drop2(out)
-        if self.down:
-            out = self.downsampler(out)
+        out = self.conv2(out)
+        out = self.norm2(out)
+        out = self.drop2(out)
+        if (self.i != self.o) or self.down:
             identity = self.downsampler(identity)
-        if self.skip:
-            if self.i == self.o:
-                out = out + identity
-            else:
-                out = torch.cat([
-                    #  [B,C      ,H,W]
-                    out[:,:self.i,:,:] + identity,
-                    out[:,self.i:,:,:] + identity,
-                ], dim=1)
+        out = out + identity
         out = nn.functional.relu(out)
         return out
 
 
 class Net(nn.Module):
     def __init__(self,
-        norm:str='bn',
-        grps:int=1,
-        drop:float=0,
+        norm:str='bn', # Normalization type
+        grps:int=1, # Normalization group count
+        drop:float=0, # Dropout rate
     ) -> None:
         super().__init__()
-        self.conv1 = conv(3, 8, norm, grps, drop)
-        self.conv2 = SkipBlock(8, 8, norm, grps, drop)
-        self.tran1 = SkipBlock(8, 8, norm, grps, drop, down=True)
-        self.conv3 = SkipBlock(8, 8, norm, grps, drop)
-        self.conv4 = SkipBlock(8, 16, norm, grps, drop)
+        self.conv1 = convblock( 3,  8, norm, grps, drop)
+        self.conv2 = SkipBlock( 8,  8, norm, grps, drop)
+        self.tran1 = SkipBlock( 8, 16, norm, grps, drop, down=True)
+        self.conv3 = SkipBlock(16, 16, norm, grps, drop)
+        self.conv4 = SkipBlock(16, 16, norm, grps, drop)
         self.conv5 = SkipBlock(16, 16, norm, grps, drop)
         self.tran2 = SkipBlock(16, 16, norm, grps, drop, down=True)
         self.conv6 = SkipBlock(16, 16, norm, grps, drop)
-        self.conv7 = SkipBlock(16, 32, norm, grps, drop)
-        self.conv8 = SkipBlock(32, 32, norm, grps, drop)
-        self.tran3 = last(32, 10)
+        self.conv7 = SkipBlock(16, 16, norm, grps, drop)
+        self.conv8 = SkipBlock(16, 32, norm, grps, drop)
+        self.tran3 = predblock(32, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
